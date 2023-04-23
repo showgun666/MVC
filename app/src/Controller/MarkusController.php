10ab601e984813e246fa1069518a18b516efb9c1
Markus Lindgren <mavn22@bth.se>
2023-04-23 17:28:17 +0200

    First commit

---
+++ app/src/Controller/MarkusController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MarkusController extends AbstractController
+{
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+    
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report.html.twig');
+    }
+
+    #[Route("/me", name: "me")]
+    public function me(): Response
+    {
+        return $this->render('me.html.twig');
+    }
+}
