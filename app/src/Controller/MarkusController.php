da5e25446cf703014de07e9cdc7850bde26f087f
Markus Lindgren <mavn22@bth.se>
2023-05-01 15:35:35 +0200

    put some php code into the lucky route

--- app/src/Controller/MarkusController.php
+++ app/src/Controller/MarkusController.php
@@ -29,7 +29,26 @@ class MarkusController extends AbstractController
     #[Route("/lucky", name: "lucky")]
     public function lucky(): Response
     {
-        // INKLUDERA DATA OCH NÅGOT SOM HÄNDER
-        return $this->render('lucky.html.twig');
+        // Lista med quotes
+        $quotes = array(
+            "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+            "The way to get started is to quit talking and begin doing. -Walt Disney",
+            "Your time is limited, don't waste it living someone else's life. -Steve Jobs",
+            "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+            "If you look at what you have in life, you'll always have more. -Oprah Winfrey",
+            "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+            "Life is what happens when you're busy making other plans. -John Lennon",
+            "Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa",
+            "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller",
+            "It is during our darkest moments that we must focus to see the light. -Aristotle"
+        );
+
+        // Slumpa ett värde ur array
+        $quote = $quotes[array_rand($quotes)];
+
+        // Skicka med quote som 'quote' vid rendering
+        return $this->render('lucky.html.twig', [
+            'quote' => $quote
+        ]);
     }
 }
